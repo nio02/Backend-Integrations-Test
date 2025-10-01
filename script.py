6b6a1b5f71848c0b26cfddd50e8f073a580e56df
NICO <annicolas2@gmail.com>
2025-10-01 15:01:26 -0500

    Added log's ip and log's host counting

--- script.py
+++ script.py
@@ -3,6 +3,11 @@
 import sys
 from datetime import datetime
 
+top_n = 5
+total_logs_counter = 0
+total_ips = {}
+total_host = {}
+
 #defining extract data function
 
 def parse_log_data(string):
@@ -22,13 +27,51 @@ def parse_log_data(string):
         "host": host
     }
 
-#Read file and extract data.
 
-def print_logs(rute):
+#Function to count how many times appear an id
+
+def count_ips(data):
+    ip_list = [d["client_ip"] for d in data]
+
+    for ip in ip_list:
+        if ip in total_ips:
+            total_ips[ip] += 1
+        else:
+            total_ips[ip] = 1
+
+def count_hosts(data):
+    host_list = [d["host"] for d in data]
+
+    for host in host_list:
+        if host in total_host:
+            total_host[host] += 1
+        else:
+            total_host[host] = 1
+
+#Read file and use data.
+
+def read_logs(rute):
+    global total_logs_counter
+    global top_n
+    chunk = 500
+    request_body = []
+
     with open(rute, "r") as f:
         for line in f:
-            print(parse_log_data(line))
+            total_logs_counter += 1
+            data = parse_log_data(line)
+            request_body.append(data)
+            if len(request_body) >= chunk:
+                count_ips(request_body)
+                count_hosts(request_body)
+                
+                #SendToApi
+
+                request_body = []
+    
+    print(total_logs_counter)
+
 
 file = sys.argv[1]
-print_logs(file)
+read_logs(file)
 
